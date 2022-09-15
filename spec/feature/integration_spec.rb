61389344d009da13a8624d24cbf0e69f40ff9660
root <root@localhost>
2022-09-15 19:03:08 +0000

    Initial commit

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,73 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('JK Rowling')
+  end
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(10)
+  end
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2020')
+  end
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('09')
+  end
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 10
+    select "2020", from: 'book_publisheddate_1i'
+    select "September", from: 'book_publisheddate_2i'
+    select "15", from: 'book_publisheddate_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('15')
   end
 end
